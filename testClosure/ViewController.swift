d147dfece2c161e6ad09bf41274dde8ac23ecdd7
iFlame <iflame@Administrators-iMac.local>
2017-05-03 10:18:06 +0530

    commit

--- testClosure/ViewController.swift
+++ testClosure/ViewController.swift
@@ -8,11 +8,65 @@
 
 import UIKit
 
+class NetworkClass  {
+    
+    func getAllProducts(completionHandler: @escaping ()->Void ){
+        
+        //DO REQUEST
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5, execute: {
+            
+            completionHandler()
+            
+            //return ["HelloThere", "HelloWorld"]
+            
+        })
+        
+    }
+    
+    
+    func testRequest(didStartHandler: @escaping ()->(), didCompleteHandler: @escaping ()->()){
+        print("Start OF FUNCTION")
+        
+        didStartHandler()
+        
+        getAllProducts(completionHandler: {
+            print("Download Finished")
+            
+            didCompleteHandler()
+        })
+        
+        print("End OF FUNCTION")
+    }
+    
+}
+
+
 class ViewController: UIViewController {
 
+    let objNet = NetworkClass()
+    let activity : UIActivityIndicatorView = UIActivityIndicatorView()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        activity.center = self.view.center
+        activity.hidesWhenStopped = true
+        activity.tintColor = UIColor.green
+        self.view.addSubview(activity)
+        
+        objNet.testRequest(didStartHandler: {
+            
+            self.activity.startAnimating()
+        },
+                           didCompleteHandler: {
+                            
+                            self.activity.stopAnimating()
+        })
     }
 
     override func didReceiveMemoryWarning() {
